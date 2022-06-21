2910b8f34a79216a11048327101ab5db616a0e79
Jason Libertelli <jason.libertelli@gmail.com>
2022-06-20 20:55:25 -0400

    added Env/updated routes

--- server/index.js
+++ server/index.js
@@ -1,19 +1,24 @@
+import 'dotenv/config';
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
 // newsest version of node allows you to use the import syntax 
 
+import postRoutes from './routes/posts.js';
+
 const app = express();
+
+app.use('/posts', postRoutes)
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-//create ENV file 
-const CONNECTION_URL = 'mongodb+srv://JLIB047:N3wYorkJ3ts22@cluster0.tvjds.mongodb.net/?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 3000;
+
+const CONNECTION_URL = process.env.CONNECTION_URL
+
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
