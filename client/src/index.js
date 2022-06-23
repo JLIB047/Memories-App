3808c6c21444aea5dbeab4203a9f9d444d01d9aa
Jason Libertelli <jason.libertelli@gmail.com>
2022-06-23 19:55:11 -0400

    read up on redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,20 @@
 import React from "react";
 import ReactDOM  from "react-dom";
+import { Provider } from "react-redux"
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+import reducers from "./reducers"
 
 import App from './App';
 
+//implement redux to make the application more scalable 
+const store = createStore(reducers, compose(applyMiddleware(thunk)))
+
 //connecting to the div w an Id of root 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
