98d175504cc780f273aa08d83538cec7ba57c2c7
Jason Libertelli <jason.libertelli@gmail.com>
2022-07-27 18:06:26 -0400

    react-router-domerror w/ Link

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { Container, Grow, Grid } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
 
 import { getPosts } from './actions/posts';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
-import memories from './images/memories.png';
 import useStyles from './styles';
+import Navbar from './components/Navbar/Navbar';
 
 const App = () => {
     const [currentId, setCurrentId] = useState(null);
@@ -18,11 +18,8 @@ const App = () => {
     }, [currentId, dispatch])
 
     return (
-        <Container maxidth='lg'>
-            <AppBar className={classes.appBar} position='static' color='inherit'>
-                <Typography className={classes.heading} variant='h2' align='center'>Memories</Typography>
-                <img className={classes.image} src={memories} alt="memories" height="60"/>
-            </AppBar>
+        <Container maxWidth='lg'>
+            <Navbar />
             <Grow in>
                 <Container>
                     <Grid className={classes.mainContainer} container justifyContent="space-between" alignItems="stretch" spacing={4}>
